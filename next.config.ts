8f3ddaf469b328795bf84385b87e7a72a81e99a6
H1p4zdev <138879774+H1p4zdev@users.noreply.github.com>
2025-05-23 01:49:26 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
+import type { NextConfig } from 'next';
 
-import type {NextConfig} from 'next';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/nextn' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/nextn' : '',
+  output: 'export',
+  basePath: isProd ? '/studio' : '',
+  assetPrefix: isProd ? '/studio/' : '',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
